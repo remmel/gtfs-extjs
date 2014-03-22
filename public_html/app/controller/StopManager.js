d86f441a15700bc57bf37c281a420148b46c5dac
remmel <remymel.web@gmail.com>
2014-03-22 16:14:45 +0100

    backend: move grid selection logic to controller

--- public_html/app/controller/StopManager.js
+++ public_html/app/controller/StopManager.js
@@ -26,14 +26,37 @@ Ext.define('CB.controller.StopManager', {
             }
         });
         
+        this.getStopsStore().on('load', this.onStoreLoad, this);
     },
+    /**
+     * When a stop is selected
+     */
     showStop: function(src, record) {
         this.getStopEdit().loadRecord(record);
-        this.getStopMap().highlightRecord(record);
+        this.getStopMap().highlightRecord(record.marker);
     },
     updateStop: function(btn){
         T = this;
         var form = this.up('form').getForm();
         console.log(form);
     },
+    /**
+     * When the store is loaded, create markers and link them with the records
+     */
+    onStoreLoad: function(store, records, options) {
+        var me = this;
+        Ext.each(records, function(record) {
+            var marker = me.getStopMap().addMarker({
+                lat: record.data.stopLat,
+                lng: record.data.stopLon,
+                title: record.data.stopName,
+                listeners: {
+                    click: function(e) {
+                        Ext.Msg.alert('It\'s fine', 'and it\'s art.');
+                    }
+                }
+            });
+            record.marker = marker;
+        }, this);
+    }
 });
--- public_html/app/view/stopmanager/Map.js
+++ public_html/app/view/stopmanager/Map.js
@@ -14,29 +14,7 @@ Ext.define('CB.view.stopmanager.Map', {
         lng: -117.133162,
         marker: {title: 'Fenway Park'}
     },
-    onStoreLoad: function(store, records, options) {
-        var me = this;
-        Ext.each(records, function(record) {
-            var marker = me.addMarker({
-                lat: record.data.stopLat,
-                lng: record.data.stopLon,
-                title: record.data.stopName,
-                listeners: {
-                    click: function(e) {
-                        Ext.Msg.alert('It\'s fine', 'and it\'s art.');
-                    }
-                }
-            });
-            record.marker = marker;
-        }, this);
-    },
-    highlightRecord: function(record) {
-        this.gmap.setCenter(record.marker.getPosition());
-    },
-    initComponent: function() {
-        var me = this;
-        var store = Ext.data.StoreManager.lookup(this.store);
-        store.on('load', this.onStoreLoad, this);
-        me.callParent(arguments);
+    highlightRecord: function(marker) {
+        this.gmap.setCenter(marker.getPosition());
     }
 });
